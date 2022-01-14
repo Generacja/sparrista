e2249e9527dddd364644922ecb25ec190c192c49
korzepadawid <korzepadawid@yahoo.com>
2022-01-14 18:57:50 +0100

    feat: matching employees

--- bot/coffee_inviter.py
+++ bot/coffee_inviter.py
@@ -1,4 +1,5 @@
 import requests
+import random
 import os
 
 from requests.exceptions import HTTPError
@@ -17,13 +18,18 @@ def invite_users():
         print(f'Other error occurred: {err}')
     else:
         employees = res.json()
-        employees.sort(key=lambda employee: employee['grade'])
+        employees.sort(
+            key=lambda employee: employee['grade']/employee['numberOfSurveys'])
         half_of_length = int(len(employees)/2)
 
         sad_employees = employees[:half_of_length]
         happy_employees = employees[half_of_length + 1:]
 
-        pass
+        sad_employee_idx = random.randrange(0, len(sad_employees))
+        happy_employee_idx = random.randrange(0, len(happy_employees))
+
+        print(sad_employees[sad_employee_idx])
+        print(happy_employees[happy_employee_idx])
 
 
 invite_users()
