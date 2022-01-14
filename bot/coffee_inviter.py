d0d597048d6ee47646215f942e4a8036dfe51b7f
korzepadawid <korzepadawid@yahoo.com>
2022-01-14 18:49:41 +0100

    feat: grouping employees

--- bot/coffee_inviter.py
+++ bot/coffee_inviter.py
@@ -1,10 +1,29 @@
 import requests
 import os
 
+from requests.exceptions import HTTPError
+
 GET_POST_COFFEE_SURVEYS = 'https://mocki.io/v1/22f4c0d0-332f-41d5-93e6-2d0ebd061293'
 
 
 def invite_users():
     """logic responsible for inviting users for a coffee"""
-    res = requests.get(GET_POST_COFFEE_SURVEYS)
-    print(res.json())
+    try:
+        res = requests.get(GET_POST_COFFEE_SURVEYS)
+        res.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        employees = res.json()
+        employees.sort(key=lambda employee: employee['grade'])
+        half_of_length = int(len(employees)/2)
+
+        sad_employees = employees[:half_of_length]
+        happy_employees = employees[half_of_length + 1:]
+
+        pass
+
+
+invite_users()
